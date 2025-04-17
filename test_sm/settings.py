525fc44898f7e911c0237c72b31712d46e6ec0e4
Surendra-58 <everestatnepal4ever@gmail.com>
2025-04-17 09:27:40 +0545

    dbsqlite

--- test_sm/settings.py
+++ test_sm/settings.py
@@ -83,12 +83,12 @@ WSGI_APPLICATION = 'test_sm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.mysql',
@@ -99,17 +99,17 @@ WSGI_APPLICATION = 'test_sm.wsgi.application'
 #         'PORT': '3306',
 #     }
 # }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bloodbank',
-        'USER': 'postgres',
-        'PASSWORD': 'suresh',
-        'HOST': 'localhost',
-        'PORT': '5432',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'bloodbank',
+#         'USER': 'postgres',
+#         'PASSWORD': 'suresh',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 
-    }
-}
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
