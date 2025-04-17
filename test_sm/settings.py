ee90085fd2eb35c0c8ac199e4f195aa56880997c
Surendra-58 <everestatnepal4ever@gmail.com>
2025-04-17 13:54:35 +0545

    setting change

--- test_sm/settings.py
+++ test_sm/settings.py
@@ -91,12 +91,12 @@ WSGI_APPLICATION = 'test_sm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 database_url = os.environ.get("DATABASE_URL")
 DATABASES["default"] = dj_database_url.parse("database_url")
 
